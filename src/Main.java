24aab1f836f4514b7a38ad76846fa819d42207cc
TV#BJJ <tv041000@gmail.com>
2023-03-12 15:02:49 +1000

    ДЗ ВЫПОЛНЕНО

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Predicate<Integer> predicate1 = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer integer) {
+                if (integer < 0) {
+                    return false;
+                }
+                return true;
+            }
+        };
+
+        Predicate<Integer> predicate2 = x -> x > 0;
+        System.out.println(predicate1.test(-100));
+        System.out.println(predicate2.test(100));
+
     }
+
 }
